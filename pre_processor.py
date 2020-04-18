bcbb44c215a7b8e24d19c8df10fbbc468dc6c3df
keshan-git <keshandesilva@gmail.com>
2020-04-18 22:10:28 +0200

    discard questions with the max length parameter

--- pre_processor.py
+++ pre_processor.py
@@ -1,8 +1,5 @@
-import numpy as np
-import tensorflow as tf
 import nltk
 import re
-import time
 import ast
 from string import punctuation
 
@@ -16,6 +13,7 @@ column_split_symbol = ' +++$+++ '
 
 # Hyper parameters
 frequent_threshold = 0.9  # to 90% of the world will be selected to generate the word2id
+max_question_size = 25  # Questions containing more than 25 words will be discarded
 
 # Import the data-set
 print('Start importing data sets')
@@ -35,7 +33,7 @@ conversation_ids = [ast.literal_eval(conversation.split(column_split_symbol)[-1]
 
 # Separate the lines in to questions and answers; answer[i] is answer to the question[i]
 # ex: ['L194', 'L195', 'L196', 'L197'] L194 is question L195 answer
-_lines = [id2line[_idx] for conversation in conversation_ids for _idx in conversation]
+_lines = [id2line[_idx] for conversation in conversation_ids for _idx in conversation][:-1]
 questions = _lines[0:][::2]
 answers = _lines[1:][::2]
 
@@ -58,13 +56,13 @@ def clean_text(text):
 
 
 print('Start cleaning questions and answers')
-clean_questions = [clean_text(question) for question in questions]
-clean_answers = [clean_text(answer) for answer in answers]
+clean_questions = {idx: clean_text(question) for idx, question in enumerate(questions)}
+clean_answers = {idx: clean_text(answer) for idx, answer in enumerate(answers)}
 
 # flat sentences to words
 print('Generating bag of words')
-bag_of_words = [word for question in clean_questions for word in question.split()] + \
-               [word for answers in clean_answers for word in answers.split()]
+bag_of_words = [word for question in clean_questions.values() for word in question.split()] + \
+               [word for answers in clean_answers.values() for word in answers.split()]
 
 # Create word distribution, so that filtering less frequent words is possible
 print('Calculating word frequency')
@@ -84,10 +82,24 @@ word2id.update({token: i for i, token in enumerate(tokens, start=1)})
 print('Creating id2word inverse mapping')
 id2word = {idx: word for word, idx in word2id.items()}
 
-# Add EOS token to all the answers
-clean_answers[:] = map(lambda answer: answer + ' ' + TOKEN_EOS, clean_answers)
-
 # Convert words in questions/answers to idx based on word2id
 print('Converting sentence in to [ids] based on word2id mapping')
-questions2id = [[word2id.get(word, word2id[TOKEN_UNK]) for word in question.split()] for question in clean_questions]
-answers2id = [[word2id.get(word, word2id[TOKEN_UNK]) for word in answer.split()] for answer in clean_answers]
+questions2id = {idx: [word2id.get(word, word2id[TOKEN_UNK]) for word in question.split()]
+                for idx, question in clean_questions.items()}
+
+answers2id = {idx: [word2id.get(word, word2id[TOKEN_UNK]) for word in answer.split()]
+              for idx, answer in clean_answers.items()}
+
+# Sort questions by the length
+print('Start sorting questions based on the word counts (=length)')
+sorted_questions2id = []
+sorted_answers2id = []
+for idx, question in sorted(questions2id.items(), key=lambda item: len(item[1])):
+    if len(question) > max_question_size:
+        break
+
+    sorted_questions2id.append(question)
+
+    # Add EOS token to all the answers
+    answers2id[idx].append(word2id[TOKEN_EOS])
+    sorted_answers2id.append(answers2id[idx])
