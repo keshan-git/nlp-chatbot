a10edc7424bca469693a51f04b4185264340a4cc
Keshan De Silva <Gusthigngna.De.Silva1@ing.com>
2020-04-18 19:53:30 +0200

    word frequency calculation and word embedding

--- pre_processor.py
+++ pre_processor.py
@@ -1,17 +1,28 @@
 import numpy as np
 import tensorflow as tf
+import nltk
 import re
 import time
 import ast
 from string import punctuation
 
+# Define special tokens
+TOKEN_PAD = '<PAD>'
+TOKEN_SOS = '<SOS>'
+TOKEN_EOS = '<EOS>'
+TOKEN_UNK = '<UNK>'
+
 column_split_symbol = ' +++$+++ '
+frequent_threshold = 0.9  # to 90% of the world will be selected to generate the word2id
 
 # Import the data-set
+print('Start importing data sets')
 lines = open('dataset/movie_lines.txt', encoding='utf8', errors='ignore').read().split(sep='\n')
 conversations = open('dataset/movie_conversations.txt', encoding='utf8', errors='ignore').read().split(sep='\n')
 contractions = open('dataset/contractions.txt', encoding='utf8', errors='ignore').read().split(sep='\n')
+print('Data sets are imported')
 
+print('Building internal data structure from the data set')
 # Create the dictionary that map each line to its line id, key=column1 value=column5
 id2line = {line.split(column_split_symbol)[0]: line.split(column_split_symbol)[4] for line in lines if
            len(line.split(column_split_symbol)) == 5}
@@ -44,5 +55,29 @@ def clean_text(text):
     return text
 
 
+print('Start cleaning questions and answers')
 clean_questions = [clean_text(question) for question in questions]
-clean_answers = [clean_text(answers) for answer in answers]
+clean_answers = [clean_text(answer) for answer in answers]
+
+# flat sentences to words
+print('Generating bag of words')
+bag_of_words = [word for question in clean_questions for word in nltk.tokenize.word_tokenize(question)] + \
+               [word for answers in clean_answers for word in nltk.tokenize.word_tokenize(answers)]
+
+# Create word distribution, so that filtering less frequent words is possible
+print('Calculating word frequency')
+frequent_distribution = nltk.FreqDist(bag_of_words)
+
+_len = len(frequent_distribution)
+print('Selecting top {0}% words out of total {1} words'.format(frequent_threshold * 100, _len))
+selected_words = frequent_distribution.most_common(int(_len * frequent_threshold))
+
+# Create word2id mapping - Word embedding
+print('Creating word2id for the selected words')
+tokens = [TOKEN_PAD, TOKEN_SOS, TOKEN_EOS, TOKEN_UNK]
+word2id = {word[0]: i for i, word in enumerate(selected_words, start=len(tokens)+1)}
+word2id.update({token: i for i, token in enumerate(tokens, start=1)})
+
+# Create id2word mapping - Inverse word mapping
+print('Creating id2word inverse mapping')
+id2word = {idx: word for word, idx in word2id.items()}
